ea029d845ce8220b140bd1854f28e24a9d2889f0
ahm3dzizo <ahm3dzizo@gmail.com>
2026-07-19 08:17:12 +0300

    update: 2026-07-19 08:17

--- src/pages/TicketDetail.tsx
+++ src/pages/TicketDetail.tsx
@@ -1004,7 +1004,7 @@ export default function TicketDetail() {
         const hasMany = lightbox.items.length > 1;
         return (
           <div
-            className="fixed inset-0 z-50 bg-black/90 flex items-center justify-center p-4"
+            className="fixed inset-0 z-[9999] bg-black/90 flex items-center justify-center p-4"
             onClick={closeLightbox}
           >
             {/* Top controls */}
